787dbb5cfbf66287b4cfdd2e844e09f4311c570c
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-09 12:32:22 +0100

    vyriesene

--- Open-Lab-03.06/Checker.cs
+++ Open-Lab-03.06/Checker.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_03._06
     {
         public bool HasSpaces(string str)
         {
-            throw new NotImplementedException();
-        }
+            for (int i = 0; i < str.length; i++)
+			{
+                if(str[i]==' '){
+
+                    return true;
+                 
+                }
+			}
+            return false;
+          
+       
+        } 
     }
 }
